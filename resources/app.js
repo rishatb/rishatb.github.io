037173cd3ecadf6a891602bb89c2859148c53042
rishatb <itsallforme10@yandex.ru>
2022-10-07 01:07:17 +0300

    portfolio

--- resources/app.js
+++ resources/app.js
@@ -1,40 +1,41 @@
-button1 = document.getElementById("one_button");
-button2 = document.getElementById("two_button");
-image = document.getElementById("image")
+// button1 = document.getElementById("one_button");
+// button2 = document.getElementById("two_button");
+// image = document.getElementById("image")
 
-textName = document.getElementById("h2_text")
-text = document.getElementById("h4_text")
+// textName = document.getElementById("h2_text")
+// text = document.getElementById("h4_text")
 
-let i = 0;
+// let i = 0;
 
-allTextName = ['3Д Моделирование', "Портфолио", 'Психология'];
-allImage = ['./resources/images/site2.png', "./resources/images/site3.png", "./resources/images/site1.png"]
-
-
-
-let Change2 = function() {
-    image.src = allImage[i];
-    textName.innerText = allTextName[i]
-    text.innerText = "Мне 20 лет, обучаюсь в МГТУ имени Никола Эрнестовича Баумана, по образованию инженер. Помимо профильного образования обучаюсь на Frontend-Developer курсе платформы Codecademy. Имею неплохой опыт в создании лендингов для различных проектов: портфолио, сайт личного"
-    if (i >= 2 || i < 0) {
-        i = 0;
-    } else {
-        i++
-    }
-}
-
-let Change1 = function() {
-
-    image.src = allImage[i];
-    textName.innerText = allTextName[i]
-    text.innerText = "Мне 80 лет, обучаюсь в МГТУ имени Никола Эрнестовича Баумана, по образованию инженер. Помимо профильного образования обучаюсь на Frontend-Developer курсе платформы Codecademy. Имею неплохой опыт в создании лендингов для различных проектов: портфолио, сайт личного"
-    if (i > 2 || i <= 0) {
-        i = 2;
-    } else {
-        i--
-    }
-}
+// allTextName = ['3Д Моделирование', "Портфолио", 'Психология'];
+// allImage = ['./resources/images/site2.png', "./resources/images/site3.png", "./resources/images/site1.png"]
 
 
-button1.onclick = Change1;
-button2.onclick = Change2;
+
+// let Change2 = function() {
+//     image.src = allImage[i];
+//     textName.innerText = allTextName[i]
+//     text.innerText = "Мне 20 лет, обучаюсь в МГТУ имени Никола Эрнестовича Баумана, по образованию инженер. Помимо профильного образования обучаюсь на Frontend-Developer курсе платформы Codecademy. Имею неплохой опыт в создании лендингов для различных проектов: портфолио, сайт личного"
+//     if (i >= 2 || i < 0) {
+//         i = 0;
+//     } else {
+//         i++
+//     }
+// }
+
+// let Change1 = function() {
+
+//     image.src = allImage[i];
+//     textName.innerText = allTextName[i]
+//     text.innerText = "Мне 80 лет, обучаюсь в МГТУ имени Никола Эрнестовича Баумана, по образованию инженер. Помимо профильного образования обучаюсь на Frontend-Developer курсе платформы Codecademy. Имею неплохой опыт в создании лендингов для различных проектов: портфолио, сайт личного"
+//     if (i > 2 || i <= 0) {
+//         i = 2;
+//     } else {
+//         i--
+//     }
+// }
+
+
+// button1.onclick = Change1;
+// button2.onclick = Change2;
+
